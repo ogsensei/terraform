3c81d63c655b6d5001964fffd755d39e8c84b3e5
OG <ojaswireply@gmail.com>
2023-05-15 12:14:15 +0530

    Updated

--- terraform-test/variable.tf
+++ terraform-test/variable.tf
@@ -1,27 +1,24 @@
-#output "printname" {
-#  value = "hello ${var.username}, you age is ${var.age}, ${var.yesorno}"
-#}
-//List output
-output "test" {
-  value = "hello world ${var.test[0]}"
-}
-
-//Function to call all the default value
-#output "test1" {
-#  value =  "${join(",",var.test)}"
+#output hello {
+#  value = "hello wolrd"
 #}
 
-//Function - upper - it is used for do capital word
-output "test1" {
-  value =  "${upper(var.test[0])}"
+#variable "username" {
+#  type = string
+#}
+#
+#variable "age" {
+#  type = number
+#}
+#
+#variable "yesorno" {
+#  type = bool
+#}
+
+//List
+
+variable "test" {
+  type = list
+  default = ["og","sam","joy","luffy"]
 }
 
-//Function - lower - it is used for do lower case for the word
-output "test2" {
-  value =  "${lower(var.test[0])}"
-}
-
-//Function - title - it is used for doing the first letter as capital
-output "test3" {
-  value =  "${title(var.test[0])}"
-}
+//Map
