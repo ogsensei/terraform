caa86a3bad0a514c74351018a676c26cbc3f0661
Ojaswi Kumar Singh <ojaswi@192.168.1.3>
2023-05-28 22:15:36 +0530

    basic

--- revision/revision.tf
+++ revision/revision.tf
@@ -8,5 +8,40 @@ output printname {
 }
 
 output "name" {
-  value = "${join(",",var.users)}"
+  value = "${join("------>",var.users)}"
+}
+
+output "upper" {
+  value = "${upper(var.users[2])}"
+}
+
+output "lower" {
+  value = "${lower(var.users[2])}"
+}
+
+output "title" {
+  value = "${title(var.users[2])}"
+  
+}
+
+#Map Variable static
+variable "map" {
+  type =  map
+  default = {
+    og = 20
+    roy = 30
+  }
+}
+
+output "age" {
+  value = "The age of the og is ${lookup(var.map,"og")}"
+}
+
+#Map Variable dynamic
+variable "userage" {
+  type = string
+}
+
+output "agedynamic" {
+  value = "The age of the ${var.userage} is ${lookup(var.map,"${var.userage}")}"
 }
